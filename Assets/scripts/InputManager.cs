d0f1c20e610ece04892e8d3e1d0fb3c2355c6341
- <->
2017-11-22 16:55:22 +0100

    player

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -4,41 +4,82 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    private PlayerBejaviour player;
+    private PlayerBehaviour player;
 
 	// Use this for initialization
 	void Start()
     {
-        player = GameObject.FindGameObjectsWithTag("Player").GetComponent<PlayerBejaviour>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
     }
-
     // Update is called once per frame
     void Update()
     {
+        // Pause game
         InputPause();
+        // Movement
         InputMovement();
+        // Jump
         InputJump();
+        // Speed
         InputSpeed();
-
     }
+
+    #region Controls
     void InputPause()
     {
         if(Input.GetButtonDown("Cancel")) Debug.Log("Pause game");
     }
+
     void InputMovement()
     {
         Vector2 axis = Vector2.zero;
-        axis.x = Input.GetAxis("Horizondal");
+        axis.x = Input.GetAxis("Horizontal");
         axis.y = Input.GetAxis("Vertical");
+        player.SetAxis(axis);
     }
-    void InputPause()
+
+    void InputJump()
     {
-        if(Input.GetButtonDown("Jump")) Player.JumpStart();
+        if(Input.GetButtonDown("Jump")) player.JumpStart();
     }
-    void InputPause()
+
+    void InputSpeed()
     {
-       // if(Input.GetButtonDown("Run")) PlayerBejaviour.running = true;
-       // if(Input.GetButtonUp("Run")) PlayerBejaviour.running = false;
+        if(Input.GetButtonDown("Run")) player.running = true;
+        else if(Input.GetButtonUp("Run")) player.running = false;
     }
+    #endregion
+
+
+/*
+        // Update is called once per frame
+        void Update()
+        {
+            InputPause();
+            InputMovement();
+            InputJump();
+            InputSpeed();
+
+        }
+        void InputPause()
+        {
+            if(Input.GetButtonDown("Cancel")) Debug.Log("Pause game");
+        }
+        void InputMovement()
+        {
+            Vector2 axis = Vector2.zero;
+            axis.x = Input.GetAxis("Horizondal");
+            axis.y = Input.GetAxis("Vertical");
+        }
+        void InputPause()
+        {
+            if(Input.GetButtonDown("Jump")) Player.JumpStart();
+        }
+        void InputPause()
+        {
+           // if(Input.GetButtonDown("Run")) PlayerBejaviour.running = true;
+           // if(Input.GetButtonUp("Run")) PlayerBejaviour.running = false;
+        }
+        */
 }
 
